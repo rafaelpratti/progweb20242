04ee1979b8ac01d01168ca135c12a1cae1ffc067
rafaelpratti <101602706+rafaelpratti@users.noreply.github.com>
2024-09-26 12:32:54 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    echo "<img src=https://picsum.photos/500/600><img> <br> <p>Atividade 1<p>"
+    ?>
+</body>
+</html>
