cb38fde92600fe79eb60680e96ce8d65406aacb6
rafaelpratti <101602706+rafaelpratti@users.noreply.github.com>
2024-09-26 12:51:15 -0300

    Add files via upload

---
+++ exercício1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    echo "URL absoluta  <br><a href=https://picsum.photos/500/600>Imagens aleatórias<a>  <br> URL relativa <br> <a href=index1.php>Imagens Aleatórias<a>"
+    ?>
+</body>
+</html>
