5dc54eaa3dbdfb7a943465a3bd4b3512e409c683
rafaelpratti <101602706+rafaelpratti@users.noreply.github.com>
2024-10-03 12:55:41 -0300

    Update index.php

--- exercicio 2/index.php
+++ exercicio 2/index.php
@@ -1,23 +1,4 @@
-<?php
 
-$datetime = new DateTime("now", new DateTimeZone("America/Sao_Paulo"));
-$hora = $datetime->format('H');
-
-
-$mensagem = '';
-$imagem = '';
-
-if ($hora >= 0 && $hora <= 12) {
-    $mensagem = "Bom dia!";
-    $imagem = "imagem_bom_dia.jpg"; 
-} elseif ($hora > 12 && $hora < 18) {
-    $mensagem = "Boa tarde!";
-    $imagem = "imagem_boa_tarde.jpg"; 
-} elseif ($hora >= 18 && $hora <= 24) {
-    $mensagem = "Boa noite!";
-    $imagem = "imagem_boa_noite.jpg"; 
-}
-?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -27,9 +8,30 @@ if ($hora >= 0 && $hora <= 12) {
     <title>Mensagem do Dia</title>
 </head>
 <body>
+
+<?php
+
+$datetime = new DateTime("now", new DateTimeZone("Asia/Dubai"));
+$hora = $datetime->format('H');
+
+
+$mensagem = '';
+$imagem = '';
+
+if ($hora >= 0 and $hora <= 12) {
+    $mensagem = "Bom dia!";
+    $imagem = "imagem_bom_dia.jpg"; 
+} elseif ($hora > 12 and $hora < 18) {
+    $mensagem = "Boa tarde!";
+    $imagem = "imagem_boa_tarde.jpg"; 
+} elseif ($hora >= 18 and $hora <= 24) {
+    $mensagem = "Boa noite!";
+    $imagem = "imagem_boa_noite.jpg"; 
+}
+?>
+
+
     <h1><?php echo $mensagem; ?></h1>
-    <?php if ($imagem): ?>
-        <img src="<?php echo $imagem; ?>" alt="<?php echo $mensagem; ?>" style="max-width: 100%; height: auto;">
-    <?php endif; ?>
+        <img src="<?php echo $imagem; ?>" alt="<?php echo $mensagem; ?>">
 </body>
 </html>
